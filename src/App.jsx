dd43d00716843a1000c5788cbd98fd326240d0c6
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-04-12 18:49:51 -0300

    Creating useReduce

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 //react
+import { useContext } from 'react'
+import { QuizContext } from './context/quiz'
 
 //components
 import Welcome from './components/Welcome/Welcome'
-
+import Questions from './components/Questions/Questions'
 //estáticos
 import './App.css'
 
 
 export function App() {
-  
+  const [quizState, dispatch] = useContext(QuizContext)  
+
   return (
     <div className='App'>
         <h1>Quiz de Programação</h1>
-        <Welcome />
+        {quizState.gameStage === "Start" && <Welcome />}
+        {quizState.gameStage === "Playing" && <Questions />}
     </div>  
   )
 }
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -7,14 +7,13 @@ import Quiz from '../../img/quiz.svg'
 import './Welcome.css'
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext)
+  const [quizState, dispatch] = useContext(QuizContext)
 
-  console.log(quizState)
   return (
     <div id="welcome">
         <h2>Seja bem-vindo</h2>
         <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
+        <button onClick={() => dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
         <img src={Quiz} alt="Início do Quiz" />
     </div>
   )
